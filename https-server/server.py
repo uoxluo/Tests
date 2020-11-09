602d1787dc30fbac4cb8bbe88ed253e4c402790b
Xianghui Luo <luoxh@mailbox.org>
2020-11-10 12:50:14 +1300

    improvements

--- https-server/server.py
+++ https-server/server.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-from http.server import HTTPServer, CGIHTTPRequestHandler
-import ssl
+from http.server import HTTPServer, SimpleHTTPRequestHandler
 
-# Start the CGIHTTP server on port 443
-httpd = HTTPServer(("", 443), CGIHTTPRequestHandler)
-httpd.socket = ssl.wrap_socket(httpd.socket, keyfile="key.pem", certfile="cert.pem", server_side=True)
+# Start the HTTP server on port 80
+httpd = HTTPServer(("", 80), SimpleHTTPRequestHandler)
+# https
+#import ssl
+#httpd.socket = ssl.wrap_socket(httpd.socket, keyfile="key.pem", certfile="cert.pem", server_side=True)
 httpd.serve_forever()
